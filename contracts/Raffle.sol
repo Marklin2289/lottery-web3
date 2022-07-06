a03680510b3c99f7dff70ba3c1de4cb0acf0e655
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-07-05 20:44:33 -0700

    initial setup

---
+++ contracts/Raffle.sol
@@ -0,0 +1,7 @@
+
+// Raffle
+
+// Enter the lottery (paying some amount)
+// Pick a random number (verifiable random)
+// Winner to be selected every X minutes => completly automated
+// Chainlink oracle -> Ransomness, Automated Excution(Chainlink Kepper)
