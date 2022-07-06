9701f06c6ffae74aec6900505d8dc2fcb60449fc
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-07-05 23:07:10 -0700

    initial Raffle.sol & work on events

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,4 +9,41 @@
 
 pragma solidity ^0.8.7;
 
-contract Raffle {}
+contract Raffle {
+    /*Error functions */
+    error Raffle__NotEnoughETHEntered();
+
+    /*State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // require(msg.value > i_entranceFee, "Not enough ETH!") // gas fee too much
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        //adding players who enter raffle into players array. (payable is important)
+        s_players.push(payable(msg.sender));
+        // Emit an event when we update a dynamic array or mapping
+        // Named events with the function name reversed
+        emit RaffleEnter(msg.sender);
+    }
+
+    // function pickRandomWinner() {}
+
+    // getter functions
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
