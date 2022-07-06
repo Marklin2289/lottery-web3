e2f610db18270168157ad4b140cf9456756386f6
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-07-05 20:48:06 -0700

    basic contract setup and yarn hardhat compile

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,7 +1,12 @@
-
 // Raffle
 
 // Enter the lottery (paying some amount)
 // Pick a random number (verifiable random)
 // Winner to be selected every X minutes => completly automated
 // Chainlink oracle -> Ransomness, Automated Excution(Chainlink Kepper)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Raffle {}
