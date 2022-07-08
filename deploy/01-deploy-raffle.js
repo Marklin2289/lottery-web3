fe34760d26b1ff794a8e4cfd4a9633753c2011d4
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-07-07 22:28:08 -0700

    added netwroks and .env info

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,1 +1,12 @@
-module.exports = async function ({ getNamedAccounts, deployments }) {}
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    //get raffle contract
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: 6,
+    })
+}
