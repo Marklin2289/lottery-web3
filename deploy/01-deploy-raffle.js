22ed6695328e7c23237581883004b9440577abd3
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-07-17 19:24:27 -0700

    "fixed verify args"

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -2,7 +2,7 @@ const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
-const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
@@ -45,7 +45,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     // Verify the deployment
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
-        await verify(raffle.address, arguments)
+        await verify(raffle.address, args)
     }
 
     log("Enter lottery with command:")
