7a5ee3322ed30b706f40b9b21344561680d4d3d9
Mark Lin <marklin-mac-mini@Marks-Mac-mini.local>
2022-07-20 22:05:58 -0700

    yarn hardhat test --network rinkeby

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -1,33 +1,33 @@
-const { network, ethers, getNamedAccounts, deployments } = require("hardhat")
-const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 const { assert, expect } = require("chai")
+const { getNamedAccounts, deployments, ethers, network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
 developmentChains.includes(network.name)
     ? describe.skip
-    : describe("Raffle Staging Test", function () {
+    : describe("Raffle Staging Tests", function () {
           let raffle, raffleEntranceFee, deployer
 
-          beforeEach(async () => {
+          beforeEach(async function () {
               deployer = (await getNamedAccounts()).deployer
               raffle = await ethers.getContract("Raffle", deployer)
               raffleEntranceFee = await raffle.getEntranceFee()
-              //   accounts = await ethers.getSigners()
           })
 
           describe("fulfillRandomWords", function () {
-              it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async () => {
+              it("works with live Chainlink Keepers and Chainlink VRF, we get a random winner", async function () {
                   // enter the raffle
+                  console.log("Setting up test...")
                   const startingTimeStamp = await raffle.getLatestTimeStamp()
                   const accounts = await ethers.getSigners()
 
-                  //   setup listener before we enter the raffle :
-                  // Just in case the blockchain moves REALLY fast
+                  console.log("Setting up Listener...")
                   await new Promise(async (resolve, reject) => {
+                      // setup listener before we enter the raffle
+                      // Just in case the blockchain moves REALLY fast
                       raffle.once("WinnerPicked", async () => {
                           console.log("WinnerPicked event fired!")
-
                           try {
-                              // add our asserts
+                              // add our asserts here
                               const recentWinner = await raffle.getRecentWinner()
                               const raffleState = await raffle.getRaffleState()
                               const winnerEndingBalance = await accounts[0].getBalance()
@@ -41,22 +41,21 @@ developmentChains.includes(network.name)
                                   winnerStartingBalance.add(raffleEntranceFee).toString()
                               )
                               assert(endingTimeStamp > startingTimeStamp)
+                              resolve()
                           } catch (error) {
                               console.log(error)
                               reject(error)
                           }
                       })
-                  })
-                  // entering the raffle :
-                  //   await raffle.enterRaffle({ value: raffleEntrance })
-                  //   const winnerStartingBalance = await accounts[0].getBalance()
-                  console.log("Entering Raffle...")
-                  const tx = await raffle.enterRaffle({ value: raffleEntranceFee })
-                  await tx.wait(1)
-                  console.log("Ok, time to wait...")
-                  const winnerStartingBalance = await accounts[0].getBalance()
+                      // Then entering the raffle
+                      console.log("Entering Raffle...")
+                      const tx = await raffle.enterRaffle({ value: raffleEntranceFee })
+                      await tx.wait(1)
+                      console.log("Ok, time to wait...")
+                      const winnerStartingBalance = await accounts[0].getBalance()
 
-                  //   this code WON'T complete until listener has finished listening:
+                      // and this code WONT complete until our listener has finished listening!
+                  })
               })
           })
       })
